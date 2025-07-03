c33ef705196d92acc7eb04f16283121ea2075b2c
Gary <gary@heavy.dev>
2025-07-03 16:55:28 +0700

    :feat: U30-16 SUE-4 Add typo on styleguide page

--- pages/styleguide.tsx
+++ pages/styleguide.tsx
@@ -1,7 +1,73 @@
 export default function Styleguide() {
   return (
-    <div className="container">
-      <h1 className="h1">Styleguide</h1>
+    <div className="container mx-auto my-8">
+      <h2 className="h2 font-sans mt-12 mb-6 uppercase">Typography</h2>
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
+        <div className="flex flex-col gap-10">
+          <div className="flex flex-col gap-2">
+            <p className="h1">
+              Right way to share it
+            </p>
+            <p className="text-body">H1</p>
+          </div>
+          <div className="flex flex-col gap-2">
+            <p className="h2">
+              Right way to share it
+            </p>
+            <p className="text-body">H2</p>
+          </div>
+          <div className="flex flex-col gap-2">
+            <p className="h3">
+              Right way to share it
+            </p>
+            <p className="text-body">H3</p>
+          </div>
+          <div className="flex flex-col gap-2">
+            <p className="h4">
+              Right way to share it
+            </p>
+            <p className="text-body">H4</p>
+          </div>
+          <div className="flex flex-col gap-2">
+            <p className="h5">
+              Right way to share it
+            </p>
+            <p className="text-body">H5</p>
+          </div>
+          <div className="flex flex-col gap-2">
+            <p className="h6">
+              Right way to share it
+            </p>
+            <p className="text-body">H6</p>
+          </div>
+        </div>
+        <div className="flex flex-col gap-10">
+          <div className="flex flex-col gap-2">
+            <p className="text-body-large">
+              Right way to share it
+            </p>
+            <p className="text-body">Text Body Large</p>
+          </div>
+          <div className="flex flex-col gap-2">
+            <p className="text-body">
+              Right way to share it
+            </p>
+            <p className="text-body">Text Body</p>
+          </div>
+          <div className="flex flex-col gap-2">
+            <p className="text-body-small">
+              Right way to share it
+            </p>
+            <p className="text-body">Text Body Small</p>
+          </div>
+          <div className="flex flex-col gap-2">
+            <p className="text-button">
+              Right way to share it
+            </p>
+            <p className="text-body">Text Button</p>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
